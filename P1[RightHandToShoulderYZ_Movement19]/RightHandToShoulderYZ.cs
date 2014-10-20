2b2b4eb8011afdb0ed82fc25da8e58f3ea3fac63
alberto__segura <alberto.segura.delgado@gmail.com>
2014-10-20 13:05:49 +0200

    initial idea and commit
    initial idea and commit

---
+++ P1[RightHandToShoulderYZ_Movement19]/RightHandToShoulderYZ.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Kinect;
+
+public class RightHandToShoulderYZ
+{
+    private Skeleton skeleton;
+
+    private detected180 = false;
+    private detected90 = false;
+    private detected0 = false;
+
+    public RightHandToShoulderYZ()
+	{
+	}
+
+    public bool detection()
+    { // Idea: to search for elbow's angle .
+        // 1º-> 180º (recto)
+        // 2º -> 90º (L)
+        // 3º -> 0º -> Movimiento completado.
+        // Using points to get vectors and vectors to get angles
+
+        Joint shoulder = skeleton.Joints[JointType.ShoulderRight];
+        Joint elbow = skeleton.Joints[JointType.ElbowRight];
+        Joint wrist = sekeleton.Joints[JointType.WristRight];
+        Joint hand = sekeleton.Joints[JointType.HandRight];
+
+        return false;
+    }
+}
